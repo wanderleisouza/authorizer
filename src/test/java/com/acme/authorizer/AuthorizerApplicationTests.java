972d7d322f25715fcb810d35e0844d5a7a9737a8
Wanderlei Souza <wandi.darko@gmail.com>
2024-05-26 15:57:03 -0300

    initial commit

---
+++ src/test/java/com/acme/authorizer/AuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

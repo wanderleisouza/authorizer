c6c2fecf8d5b02d69e5d1a4df9f4000828922136
Wanderlei Souza <wandi.darko@gmail.com>
2024-05-26 21:01:06 -0300

    simple example for service calls and spring events

--- src/test/java/com/acme/authorizer/AuthorizerApplicationTests.java
+++ src/test/java/com/acme/authorizer/AuthorizerApplicationTests.java
@@ -3,21 +3,19 @@ package com.acme.authorizer;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
-import org.springframework.modulith.test.ApplicationModuleTest;
 
-@ApplicationModuleTest
 class AuthorizerApplicationTests {
+
+	ApplicationModules modules = ApplicationModules.of(AuthorizerApplication.class);
 
 	@Test
 	void moduleContextVerify() {
-		var modules = ApplicationModules.of(AuthorizerApplication.class);
 		modules.forEach(System.out::println);
-		ApplicationModules.of(AuthorizerApplication.class).verify();
+		modules.verify();
 	}
 
 	@Test
 	void writeDocumentationSnippets() {
-		var modules = ApplicationModules.of(AuthorizerApplication.class);
 		new Documenter(modules)
 				.writeModuleCanvases()
 				.writeModulesAsPlantUml()
